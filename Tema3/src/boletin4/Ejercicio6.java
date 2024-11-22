a1d8b89fcd96c5382c344911cf72acb6005df4c8
marioamado-code <mario.amado@iesnervion.es>
2024-11-22 14:30:42 +0100

    ejercicio 6 modificado

--- Tema3/src/boletin4/Ejercicio6.java
+++ Tema3/src/boletin4/Ejercicio6.java
@@ -11,29 +11,36 @@ public class Ejercicio6 {
 		int t[] = { 12, 11, 13, 14, 2, 6 };
 		
 		// creamos la variable suma de n numeros
-		
-		// creamos la variable n numeros
 		int salto;
 		
-		int suma = 0;
+		
 		// Creamos el escaner
 		Scanner sc = new Scanner(System.in);
 		// pedimos de cuanto será la agrupación
 		System.out.println("de cuanto será la agrupación");
 		// Lo leemos
 		salto = sc.nextInt();
-		int tf[] = new int[t.length-(salto-1)];
-		// Hacemos un for para ir pasando de conjunto en conjunto
-		for (int h = 0; h < t.length;h++) {
-		while(h<=salto+(1*h)) {
-			suma+=t[h];
-		}
-		System.out.println(suma);	
-		}
-
+		
+		// creamos la variable n numeros
+				int resultado[]=suma(t,salto);
 		// Imprimimos la tabla
-		System.out.print(Arrays.toString(tf));
-		sc.close();
-	}
+				System.out.print(Arrays.toString(resultado));
+				sc.close();
+			}
+	static int[] suma(int t[], int numElementos) {
+		// el tamaño de la tabla resultante es longitud - numElementos +1
+		int[] result = new int[t.length - numElementos + 1];
 
+		// Recorremos la tabla resultante y así nos aseguramos que no nos salimos de los
+		// límites
+		for (int i = 0; i < result.length; i++) {
+			// Las posiciones que tengo que sumar del array van desde i (posición de la
+			// resultante que tengo que rellenar) hasta i+numElementos-1
+			for (int j = i; j < i + numElementos; j++) {
+				result[i] += t[j];
+			}
+		}
+
+		return result;
+	}
 }
